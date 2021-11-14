6bc238fa0ec00a828ee9659d92aac937cea11527
Prerit Munjal <77001439+PreritMunjal@users.noreply.github.com>
2021-11-14 23:33:02 +0530

    Update program1.cpp

--- Week6/program1.cpp
+++ Week6/program1.cpp
@@ -1,1 +1,50 @@
+#include <iostream>
+#include <stack>
+#define MAX 20
+using namespace std;
+bool isPath(int *G[MAX], int V, int s, int d) {
+    stack<int> st;
+    bool *visited = new bool[V]();
+    st.push(s);
+    visited[s] = 1;
+    while (!st.empty()) {
+        int u = st.top();
+        st.pop();
+        for (int v = 0; v < V; ++v) 
+        {
+            if (G[u][v] && v == d)
+                return true;
+            if (G[u][v] && !visited[v])
+            {
+                st.push(v);
+                visited[v] = 1;
+            }
+        }
+    }
 
+    return false;
+}
+int main() {
+    int t, v, s, d;
+    cout<<"Enter the number of test cases:";
+    cin >> t;
+    while (t--) {
+        cout<<"Enter the size of array:";
+        cin >> v;
+        int **G = new int*[v];
+        cout<<"Enters the elements of array:";
+        for (int i = 0; i < v; ++i) {
+            G[i] = new int[v];
+            for (int j = 0; j < v; ++j)
+                cin >> G[i][j];
+        }
+        cin >> s >> d;
+        if (isPath(G, v, s, d))
+            cout << "Yes, path exists";
+        else
+            cout << "No, path doesn't exist";
+        cout << endl;
+        delete[] G;
+    }
+    return 0;
+}
