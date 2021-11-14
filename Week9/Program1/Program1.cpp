dc520029e1f031c7c912201a9316fcfce2685b56
Prerit Munjal <77001439+PreritMunjal@users.noreply.github.com>
2021-11-14 23:39:24 +0530

    Update Program1.cpp

--- Week9/Program1/Program1.cpp
+++ Week9/Program1/Program1.cpp
@@ -1,1 +1,54 @@
+#include<bits/stdc++.h>
+using namespace std;
 
+#define  V 5
+#define INF 9999
+
+void printsolution(int dist[][V]);
+void floydwarshall(int graph[][V])
+{
+    int dist[V][V],i,j,k;
+    for(i=0;i<V;i++)
+     for(j=0;j<V;j++)
+        dist[i][j]=graph[i][j];
+    for(k=0;k<V;k++)
+    {
+        for(i=0;i<V;i++)
+        {
+          for(j=0;j<V;j++)
+          {
+              if(dist[i][j]>(dist[i][k] +dist[k][j]) &&(dist[k][j] != INF && dist [i][k] !=INF))
+              dist[i][j] =dist[i][k] +dist[k][j];
+          }
+        }
+     
+    }
+    printsolution(dist);
+}
+void printsolution(int dist[][V])
+{
+    cout<<"matrix shows path \n";
+    for(int i=0;i<V;i++)
+    {
+        for(int j=0;j<V;j++)
+        {
+            if(dist[i][j] == INF)
+            cout<<"INF"<<" ";
+            else
+            cout<<dist[i][j]<< " ";
+        }
+        cout<< endl;
+    }
+}
+int main()
+{
+    int graph[V][V]= { {0,10,5,5,INF},
+                       {INF,0,5,5,5},
+                       {INF,INF,0,INF,10},
+                       {INF,INF,INF,0,20},
+                       {INF,INF,INF,5,0 } };
+        
+                      
+    floydwarshall(graph);
+    return 0;
+}
