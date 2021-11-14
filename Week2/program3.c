1b3085c26de9e05a9176d9e2c7e6d07239e33467
Prerit Munjal <77001439+PreritMunjal@users.noreply.github.com>
2021-11-14 21:58:23 +0530

    Update program3.c

--- Week2/program3.c
+++ Week2/program3.c
@@ -1,1 +1,34 @@
-
+#include<stdio.h>
+#define SIZE 100
+int countPairsWithDiffK(int arr[],int n, int k)
+{
+    int count=0;
+    for(int i=0;i<n;i++)
+    {
+        for(int j=i+1;j<n;j++)
+        {
+            if(arr [i]-arr[j]== k || arr[j]-arr[i]==k)
+            count++;
+        }
+    } 
+    printf("Count is %d  \n",count);
+}
+int main()
+{
+    int arr[SIZE],n,k,test;
+printf("Enter the number of test cases:\n");
+scanf("%d",&test);
+for(int j=0;j<test;j++)
+{
+    printf("Enter the number of elements:\n ");
+    scanf("%d",&n);
+    printf("Enter the elements:\n");
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    printf("enter the value :\n");
+    scanf("%d",&k);
+    countPairsWithDiffK(arr, n, k);
+}
+}
