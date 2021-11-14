20bbedd878b579956b005c123099ffe2d7257d14
Prerit Munjal <77001439+PreritMunjal@users.noreply.github.com>
2021-11-14 23:43:23 +0530

    Update program2.cpp

--- Week11/Program2/program2.cpp
+++ Week11/Program2/program2.cpp
@@ -1,1 +1,26 @@
-
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int count(int ind,int sum,int s[],int n)
+{
+    if(sum==0)
+    return 1;
+    if(ind>=n || sum<0)
+    return 0;
+    
+    int left= count(ind,sum-s[ind],s,n);
+    int right= count(ind+1,sum,s,n);
+    return left+right;
+}
+int main()
+{
+    int s[15];
+    int sum,n;
+    cin>>n;
+    for(int i=0;i<n;i++)
+    {
+        cin>>s[i];
+    }
+    cin>>sum;
+    cout<<count(0,sum,s,n);
+}
