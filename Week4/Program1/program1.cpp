e74401922157cdf09fc87547e8a713f6fa354756
Prerit Munjal <77001439+PreritMunjal@users.noreply.github.com>
2021-11-14 22:14:57 +0530

    Update program1.cpp

--- Week4/Program1/program1.cpp
+++ Week4/Program1/program1.cpp
@@ -1,20 +1,22 @@
 #include <bits/stdc++.h>
 using namespace std; 
-
 int s=0,c=0;
-void swap(int* a,int* b) {
+void swap(int* a,int* b) 
+{
     s++; 
     int t=*a; 
     *a=*b; 
     *b=t; 
 } 
-
-int partition (int arr[], int low, int high) { 
+int partition (int arr[], int low, int high)
+{ 
     int pivot=arr[high]; 
     int i=(low-1); 
-    for (int j=low;j<=high-1;j++) { 
+    for (int j=low;j<=high-1;j++) 
+    { 
         c++;
-        if (arr[j]<pivot) {
+        if (arr[j]<pivot)
+        {
             i++; 
             swap(&arr[i],&arr[j]); 
         } 
@@ -22,21 +24,25 @@ int partition (int arr[], int low, int high) {
     swap(&arr[i+1],&arr[high]); 
     return (i+1); 
 } 
-
-void quickSort(int arr[],int low,int high) { 
-    if (low<high) {
+void quickSort(int arr[],int low,int high)
+{ 
+    if (low<high) 
+    {
         int piv = partition(arr,low,high);  
         quickSort(arr,low,piv-1); 
         quickSort(arr,piv+1,high); 
     } 
 } 
-  
 int main(){
     int n,i,t;
+    cout<<"Enter the number of test cases:";
     cin>>t; 
-    while(t--){
+    while(t--)
+    {
+        cout<<"Enter the size of array:";
         cin>>n; 
         int arr[n]; 
+        cout<<"Enter the elements:";
         for(i=0;i<n;i++){
             cin>>arr[i];
         }
